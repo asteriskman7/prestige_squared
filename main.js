8bff5fc2044397b2c2aedbe5d9db9bc9c63423de
asteriskman7 <asteriskman@gmail.com>
2017-08-17 20:13:37 -0400

    Added chat queue and began staging

--- main.js
+++ main.js
@@ -8,9 +8,12 @@ var game = {
   eTotal: undefined,
   eBuy: undefined,
   lastSpeaker: undefined,
+  chatQueue: [],
+  chatCloseCallback: undefined,
   init: function() {
     console.log('init');
-    game.initGameContainers()
+    document.getElementById('button_close_chat').onclick = game.closeChat;
+    game.initGameContainers();
   },
   initGameContainers: function() {
     var i;
@@ -40,7 +43,7 @@ var game = {
       c.style.backgroundColor = color;
       c.onclick = ((j) => function(e) {game.selectGame(e,j);})(i);
     }
-    game.games[0].activate();
+    game.games[6].activate();
     var closeButtons = document.getElementsByClassName('button_close');
     var button;
     for (i = 0; i < closeButtons.length; i++) {
@@ -55,6 +58,8 @@ var game = {
     game.load();
 
     game.offlineGains();
+
+    game.setStage(game.state.stage);
 
     setInterval(game.loop, 1000);
     //setInterval(game.save, 10000);
@@ -76,13 +81,16 @@ var game = {
         c.style.top = Math.floor(i / 3) * 33.3 + '%';
       }
     }
-    if (gameNum === -1) {
+    if ((gameNum === -1) && (e !== undefined)) {
       e.stopPropagation();
     }
   },
   loop: function() {
     var deltaTime;
     var timestamp = Date.now();
+
+    game.handleChatQueue();
+
     if (game.state.lastLoopTime !== undefined) {
       deltaTime = timestamp - game.state.lastLoopTime;
     } else {
@@ -138,6 +146,7 @@ var game = {
     } else {
       game.state = {};
       game.state.metacoins = 0;
+      game.state.stage = 'init';
     }
   },
   reset: function() {
@@ -149,29 +158,86 @@ var game = {
     fc.style.filter = 'blur(5px)';
     var o = document.getElementById('overlay');
     o.style.display = 'flex';
+    document.getElementById('button_close_chat').style.display = 'none';
   },
   hideOverlay: function() {
     var fc = document.getElementById('full_container');
     fc.style.filter = '';
     var o = document.getElementById('overlay');
     o.style.display = 'none';
+    game.clearChatMsg();
   },
   addChatMsg: function(name, msg) {
+    game.showOverlay();
     var otf = document.getElementById('overlay_text_flow');
-    var speakerAvatars = {asterisk_man: 'one_40x40.png', uni: 'two_40x40.png'};
-    var speakerColors = {asterisk_man: 'yellow', uni: 'purple'};
+    var speakerAvatars = {asterisk: 'asterisk_256.png', uni: 'uni_256.png', makiki: 'makiki_256.png'};
+    var speakerNames = {asterisk: 'asterisk_man&#x1f419;', uni: 'Uni', makiki: 'Makiki&#x1f411; (sheep cheese is tasty)' }
+    var speakerColors = {asterisk: '#ffec0b', uni: '#b870c2', makiki: '#010101'};
     var newHTML = '<div>';
     var newSpeaker = game.lastSpeaker !== name;
     game.lastSpeaker = name;
     if (newSpeaker) {
-      newHTML += '<div style="color: ' + speakerColors[name] + ';" class="text_name_line"><img src="' + speakerAvatars[name] + '">' + name + '</div>';
+      newHTML += '<div style="color: ' + speakerColors[name] + ';" class="text_name_line"><img src="' +
+                 speakerAvatars[name] + '" class="img_avatar">' + speakerNames[name] + '</div>';
     }
     newHTML += '<div class="text_text_box">' + msg + '</div></div>';
     otf.innerHTML += newHTML;
   },
+  delayChat: function(msgList, callback) {
+    //msgList is [{delay:, name:, msg:},...], delays are relative to the call time but must be increasing
+    var lastTime = Date.now();
+
+    msgList.forEach((v) => {
+      game.chatQueue.push({name: v.n, msg: v.m, time: lastTime + v.d});
+      lastTime += v.d;
+    });
+    game.chatCloseCallback = callback;
+  },
+  handleChatQueue: function() {
+    var now = Date.now();
+    var chat;
+    while ((game.chatQueue.length > 0) && (game.chatQueue[0].time <= now)) {
+      chat = game.chatQueue.shift();
+      game.addChatMsg(chat.name, chat.msg);
+      if (game.chatQueue.length === 0) {
+        document.getElementById('button_close_chat').style.display = 'block';
+      }
+    }
+  },
   clearChatMsg: function() {
     var otf = document.getElementById('overlay_text_flow');
     otf.innerHTML = '';
+  },
+  closeChat: function() {
+    game.hideOverlay();
+    if (game.chatCloseCallback !== undefined) {
+      game.chatCloseCallback();
+    }
+  },
+  setStage: function(stage) {
+    game.state.stage = stage;
+    switch (stage) {
+      case 'init':
+        document.getElementById('game_container_6').style.display = 'block';
+        document.getElementById('button_close_game6').style.display = 'none';
+        if (stage === 'init') {
+          game.selectGame(undefined, 6);
+          game.delayChat([
+            {d: 2000, n: 'makiki', m: 'Is this just a reskin of prestige?'},
+            {d: 2000, n: 'uni', m: 'I hope so. Best game ever'},
+            {d: 2000, n: 'asterisk', m: "It's not a reskin. It's got more content."}
+          ], () => {game.setStage('expand')});
+          break;
+        }
+      case 'expand':
+        game.selectGame(undefined, -1);
+        for (var i = 0; i < 9; i++) {
+          document.getElementById('game_container_' + i).style.display = 'block';
+        }
+        if (stage === 'expand') {
+          break;
+        }
+    }
   }
 
 };
@@ -181,10 +247,11 @@ game.init();
 /*
 setTimeout(() => {
   game.showOverlay();
-  game.addChatMsg('asterisk_man', 'Hello');
+  game.addChatMsg('asterisk', 'Hello');
   game.addChatMsg('uni', 'Good morning');
-  game.addChatMsg('asterisk_man', 'Welcome to the game');
-  game.addChatMsg('asterisk_man', 'I hope you enjoy this game. It takes a long time to write and this line should hopefully wrap.');
+  game.addChatMsg('asterisk', 'Welcome to the game');
+  game.addChatMsg('asterisk', 'I hope you enjoy this game. It takes a long time to write and this line should hopefully wrap.');
   game.addChatMsg('uni', 'It does..I hope.');
+  game.addChatMsg('makiki', 'hello everyone.');
 }, 1000)
 */
